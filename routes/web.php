48e6a8373ce82a6749bf861a978cbd0e2b32e39d
Sulthan A Zhafran <sulthanarifzhafran3@gmail.com>
2026-08-04 15:16:00 +0700

    refactor: remove Filament admin panel

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('pages.home');
+})->name('home');
+
+Route::view('/about', 'pages.about')->name('about');
+
+Route::view('/explore', 'pages.explore')->name('explore');
+
+Route::view('/impact', 'pages.impact')->name('impact');
+
+Route::view('/contact', 'pages.contact')->name('contact');
