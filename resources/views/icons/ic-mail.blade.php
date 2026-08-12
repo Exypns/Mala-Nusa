d08becb1b4ac2ba99b6bc9c274322d05d335fdb4
Sulthan A Zhafran <sulthanarifzhafran3@gmail.com>
2026-08-12 22:57:24 +0700

    add icon assets

---
+++ resources/views/icons/ic-mail.blade.php
@@ -0,0 +1,9 @@
+<svg 
+    class="{{ $class ?? '' }}" 
+    viewBox="1.25 2.25 15.5 13.5" 
+    fill="none" 
+    xmlns="http://www.w3.org/2000/svg"
+>
+    <path d="M13.6875 3.1875H4.3125C3.06986 3.1875 2.0625 4.19486 2.0625 5.4375V12.5625C2.0625 13.8051 3.06986 14.8125 4.3125 14.8125H13.6875C14.9301 14.8125 15.9375 13.8051 15.9375 12.5625V5.4375C15.9375 4.19486 14.9301 3.1875 13.6875 3.1875Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M2.0625 6L8.37375 8.8995C8.57019 8.98976 8.78382 9.03648 9 9.03648C9.21618 9.03648 9.42981 8.98976 9.62625 8.8995L15.9375 6" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
