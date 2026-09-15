396b51b312818ace09fb48f01a2dc6bc7ea8f701
Sulthan A Zhafran <sulthanarifzhafran3@gmail.com>
2026-09-15 21:27:07 +0700

    feat: add new routes

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,22 @@
 <?php
 
+use App\Http\Controllers\ContactSubmissionController;
+use App\Http\Controllers\ExperienceController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('pages.home');
-})->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::view('/about', 'pages.about')->name('about');
 
-Route::view('/explore', 'pages.explore')->name('explore');
+Route::get('/explore', [ExperienceController::class, 'index'])
+    ->name('explore');
+
+Route::get('/explore/{experience:slug}', [ExperienceController::class, 'show'])
+    ->name('explore.show');
 
 Route::view('/impact', 'pages.impact')->name('impact');
 
 Route::view('/contact', 'pages.contact')->name('contact');
+
+Route::post('/contact', [ContactSubmissionController::class, 'store'])->name('contact.store');
